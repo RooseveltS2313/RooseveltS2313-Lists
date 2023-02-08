62876e4769a234de433ca3d73102ae89f6cc39d4
Roosevelt Seymour <RSeymourJR04@outlook.com>
2023-02-08 15:45:14 +0000

    Completed Lists R Seymour Jr

--- src/main/java/ListExercise.java
+++ src/main/java/ListExercise.java
@@ -30,7 +30,9 @@ public class ListExercise {
      * @return a List<Integer> object.
      */
     public List<Integer> createList(){
-        return null;
+        //LinkedList is not imported so ArrayList it is
+        List<Integer> list = new ArrayList<>(); 
+        return list;
     }
 
     /**
@@ -40,7 +42,7 @@ public class ListExercise {
      * @return the size of List (number of items it holds.)
      */
     public int getSize(List<Integer> list){
-        return 0;
+        return list.size();
     }
 
     /**
@@ -52,6 +54,7 @@ public class ListExercise {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void addToList(List<Integer> list, int value){
+        list.add(value);
     }
 
     /**
@@ -67,6 +70,13 @@ public class ListExercise {
      * @return the int at the location in 'list' represented by 'index'.
      */
     public int get(List<Integer> list, int index){
+        // The if statement is to limit attempts to access indexes which do not exist in the ArrayList.
+        // It is not checked here but is something that I figure would be handled in real applications 
+        if (index < list.size() && index >= 0){
+            return list.get(index);
+        } else {
+            System.out.println("That index is not assigned in this List.");
+        }
         return 0;
     }
 
@@ -80,7 +90,8 @@ public class ListExercise {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void removeFromList(List<Integer> list, int position){
-
+        // the if statement is to limit attempts to modify indexes which do not exist in the ArrayList
+        list.remove(position);       
     }
 
     /**
@@ -93,5 +104,6 @@ public class ListExercise {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void updateAtPosition(List<Integer> list, int position, int value){
+        list.set(position, value);
     }
 }
